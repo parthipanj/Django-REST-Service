7f5ab98927e9b9515170db507f14a214b0854a44
Parthi J <parthij15@gmail.com>
2020-05-12 07:45:38 +0530

    Snippets tutorial work

--- myservice/urls.py
+++ myservice/urls.py
@@ -15,17 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
-from myservice.myapp.views import UserViewSet, GroupViewSet
 
-router = routers.DefaultRouter()
-router.register(r'users', viewset=UserViewSet)
-router.register(r'groups', viewset=GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include('snippets.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
